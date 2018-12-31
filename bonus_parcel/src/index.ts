e7cacd3be4b5124315aa5316e93e47755cede7e9
Antonio <antomunozdev@gmail.com>
2018-12-31 14:12:52 +0100

    bonus app with parcel

---
+++ bonus_parcel/src/index.ts
@@ -0,0 +1,5 @@
+import './styles.scss';
+import logoImg from './content/logo_2.png';
+const img = document.createElement('img');
+img.src = logoImg;
+document.getElementById('logo').appendChild(img);
